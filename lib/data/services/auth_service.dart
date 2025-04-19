ead8ffbd05392b6884dd1119d6ca92b3450d654c
Refliyantokasim <refliyanto.mhs22@ti.poligon.ac.id>
2025-04-19 14:11:26 +0800

    update

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class AuthService {
   final String baseUrl =
-      'http://10.0.2.2:8000/api'; // ganti dengan URL API-mu (IP emulator)
+      'http://10.0.2.2:8000/api'; // Ganti dengan URL API-mu (IP emulator)
 
   Future<Map<String, dynamic>> login({
     required String email,
